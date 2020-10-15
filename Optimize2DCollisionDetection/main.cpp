c04e8949063ba1c21e3e0829b95ea0a3e84dd923
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-10-15 23:55:46 +0900

    range_traits

--- Optimize2DCollisionDetection/main.cpp
+++ Optimize2DCollisionDetection/main.cpp
@@ -2,16 +2,22 @@
 #include"timer.hpp"
 #include"vec2_traits.hpp"
 #include"vec2.hpp"
+#include"range_traits.hpp"
+#include<vector>
 
 int main()
 {
 
-	constexpr my::vec2f a{ 1.f,2.f };
-	constexpr my::vec2f b{ 4.f,5.f };
+	constexpr my::vec2f v1{ 1.f,2.f };
+	constexpr my::vec2f v2{ 4.f,1.f };
+	constexpr my::vec2f v3{ 5.f,3.f };
+	constexpr my::vec2f v4{ 2.f,4.f };
 
-	constexpr float c = my::vec2f::dot(a, b);
+	std::vector shape{ v1,v2,v3,v4 };
 
-	static_assert(my::vec2f::dot(a, b));
+	auto [l, r, b, t] = my::range_traits<std::vector<my::vec2f>>::get_range(shape);
+
+	std::cout << l << " " << r << " " << b << " " << t << std::endl;
 
 	return 0;
 }
