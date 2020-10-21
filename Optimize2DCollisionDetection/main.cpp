4876e021a36f21c7fe06fbeddc949692ba47a8be
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-10-21 21:34:47 +0900

    draw

--- Optimize2DCollisionDetection/main.cpp
+++ Optimize2DCollisionDetection/main.cpp
@@ -7,11 +7,11 @@
 #include"tester.hpp"
 #include"load_data.hpp"
 #include"window/window.hpp"
+#include"window/draw.hpp"
 
 int main()
 {
-
-	/*
+	
 	std::string file_name;
 	std::cout << "test data file_name: ";
 	std::cin >> file_name;
@@ -22,41 +22,33 @@ int main()
 
 	auto [data, left, right, bottom, top] = my::load_data(file_name);
 
+
 	my::tester<my::tree_imple001<my::shape, my::cd_001<my::shape>>> tester{ tree_level,left,right,bottom,top };
 	tester.set_data(data);
 
-	auto [t,cnt] = tester.do_test();
+	auto [t, cnt] = tester.do_test();
 	std::cout << "\n";
 
 	for (size_t i = 0; i < cnt.size(); i++)
 		std::cout << "cnt " << i << ": " << cnt[i] << "\n";
 	std::cout << "\n";
 
-	std::cout << "time: "<<t << "\n";
-	*/
+	std::cout << "time: " << t << "\n";
 
-	auto hwnd = my::create_simple_window(L"aaa", 500.f, 500.f);
+
+	constexpr float window_width = 600.f;
+	constexpr float window_height = 600.f;
+
+	auto hwnd = my::create_simple_window(L"view", window_width, window_height);
 	auto [hdc, hglrc] = my::init_opengl(hwnd);
 
-	wglMakeCurrent(hdc, hglrc);
-
-	glClearColor(0.2, 0.2, 0.2, 0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glColor3d(1.0, 0.0, 0.0);
-	glBegin(GL_POLYGON);
-	glVertex2d(-0.9, -0.9);
-	glVertex2d(0.9, -0.9);
-	glVertex2d(0.9, 0.9);
-	glVertex2d(-0.9, 0.9);
-	glEnd();
-
-	SwapBuffers(hdc);
-	wglMakeCurrent(NULL, NULL);
+	my::draw<my::cd_001<my::shape>>(hdc, window_width, window_height, data, left, right, bottom, top);
 
 	while (my::process_window_message());
 
 	my::shutdown(hwnd, hdc, hglrc);
+
+
 
 	return 0;
 }
--- Optimize2DCollisionDetection/window/draw.hpp
+++ Optimize2DCollisionDetection/window/draw.hpp
@@ -1,11 +1,72 @@
 #pragma once
 #include"../shape/shape.hpp"
+#include<Windows.h>
+#include<glew.h>
+#include<set>
+
+#pragma comment(lib, "OpenGL32.lib")
+#pragma comment(lib, "winmm.lib")
+
 
 namespace my
 {
 	template<typename CollisionDetectionPolicy>
-	void draw(const std::vector<shape>& shape, float left, float right, float bottom, float top)
+	void draw(HDC hdc,float width, float height, const std::vector<shape>& shape, float left, float right, float bottom, float top)
 	{
+		//•`Ê‚Ì‚Â‚¢‚Å‚É“–‚½‚Á‚½‰ñ”‚ğ”‚¦o—Í
+		//second‚æ‚èfirst‚Ì•û‚ª¬‚³‚È“Y‚¦š
+		std::set<std::pair<unsigned int, unsigned int>> cnt{};
+
+		glClearColor(1.0, 1.0, 1.0, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		for (size_t i = 0; i < shape.size(); i++)
+		{
+			size_t j;
+			unsigned int left_index, right_index;
+			bool is_hit = false;
+			for (j = 0; j < shape.size(); j++)
+				if (i != j)
+					//“–‚½‚Á‚Ä‚¢‚½ê‡
+					if (CollisionDetectionPolicy::collision_detection(shape[i], shape[j])) {
+						left_index = (i < j) ? i : j;
+						right_index = (i < j) ? j : i;
+
+						//‘}“ü
+						cnt.insert({ left_index,right_index });
+
+						is_hit = true;
+					}
+						
+
+			//“–‚½‚Á‚Ä‚¢‚éê‡
+			if (is_hit) 
+				glColor3d(1.0, 0.0, 0.0);
+			//“–‚½‚Á‚Ä‚È‚¢ê‡
+			else
+				glColor3d(0.0, 0.0, 0.0);
+
+			glLineWidth(3.f);
+
+			float x, y;
+			float x_length = right - left;
+			float y_length = top - bottom;
+			
+			glBegin(GL_LINE_LOOP);
+			for (const auto& v : shape[i])
+			{
+				x = (v.x - left) / x_length * 2.f - 1.f;
+				y = (v.y - bottom) / y_length * 2.f - 1.f;
+				glVertex2d(x, y);
+			}
+			glEnd();
+			
+		}
+
+		SwapBuffers(hdc);
+		wglMakeCurrent(NULL, NULL);
+
+		std::cout << "hit cnt: " << cnt.size() << "\n";
 
 	}
 }
