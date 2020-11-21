71cf5f56e3a165c09484023f153188225353625e
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-11-21 16:54:38 +0900

    inline省略

--- CollisionDetectionLib/include/traits.hpp
+++ CollisionDetectionLib/include/traits.hpp
@@ -8,12 +8,12 @@ namespace collision_detection
 	struct shape_traits
 	{
 		//頂点数の取得
-		inline static unsigned int get_vertex_num(const T&);
+		static unsigned int get_vertex_num(const T&);
 
 		//Tの頂点のx,y座標を取得
 		//0からスタートで(vertex_num-1)までの数字がvertexNumの値として使用される
-		inline static float get_vertex_x(const T&, unsigned int vertexNum);
-		inline static float get_vertex_y(const T&, unsigned int vertexNum);
+		static float get_vertex_x(const T&, unsigned int vertexNum);
+		static float get_vertex_y(const T&, unsigned int vertexNum);
 	};
 
 	//木の登録に使用
@@ -23,6 +23,6 @@ namespace collision_detection
 		//Tが完全に収まるできるだけ小さい範囲を返す
 		//left,right,bottom,topの順
 		//テンプレートを特殊化して使用
-		inline static std::tuple<float, float, float, float> get_range(const T&);
+		static std::tuple<float, float, float, float> get_range(const T&);
 	};
 }
